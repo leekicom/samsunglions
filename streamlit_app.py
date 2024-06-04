53ff220efeed7f0ee115b2753228d4b9b2d22eda
leekicom <89513120+leekicom@users.noreply.github.com>
2024-06-04 16:57:22 +0900

    hh

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,16 @@ import altair as alt
 import numpy as np
 import pandas as pd
 import streamlit as st
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import NoSuchElementException
+from bs4 import BeautifulSoup
+
+service=webdriver.chrome.service.Service(ChromeDriverManager().install())
+driver=webdriver.Chrome(service=service)
+driver.maximize_window()
 
 """
 # Samsung Lions!!
@@ -9,5 +19,5 @@ import streamlit as st
 
 """
 
-html_string='https://m.sports.naver.com/game/20240604SSSK02024'
-st.markdown(html_string, unsafe_allow_html=True)
+url='https://m.sports.naver.com/game/20240604SSSK02024'
+driver.get(url)
